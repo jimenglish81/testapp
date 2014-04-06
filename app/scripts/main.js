6636890d23249b4ba9129720ddff8200e824c7b3
jenglish81 <jimenglish81@gmail.com>
2014-04-06 12:31:51 +0100

    whitespace

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,10 @@
 require.config({
-	paths: {
-		"jquery": "vendor/jquery/dist/jquery"
-	}
+   paths: {
+      "jquery": "vendor/jquery/dist/jquery"
+   }
 })
 
 require(['app'], function(App) {
-	// TODO - app dynamically get its modules
-	(new App());
+   // TODO - app dynamically get its modules
+   (new App());
 });
