6331574f32b7db707e9720a64bf91bcf45813ccc
jimenglish81 <jimenglish81@gmail.com>
2014-04-14 08:41:50 +0100

    Update gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
    // Project configuration.
    grunt.initConfig({
       pkg: grunt.file.readJSON('package.json'),
-  	   requirejs: {
+      requirejs: {
          compile: {
             options: {
 	            // build dir
